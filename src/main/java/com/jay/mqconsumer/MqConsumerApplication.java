0e1f0d55635574df7701ce41cfa6644370801180
ryehay <ryegonzalezphi1.618@gmail.com>
2022-09-09 20:40:44 +0800

    Applied fix for 'application unable to deploy to server' issue

--- src/main/java/com/jay/mqconsumer/MqConsumerApplication.java
+++ src/main/java/com/jay/mqconsumer/MqConsumerApplication.java
@@ -2,11 +2,13 @@ package com.jay.mqconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MqConsumerApplication {
+public class MqConsumerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+		System.setProperty("server.servlet.context-path", "/mq-consumer");
 		SpringApplication.run(MqConsumerApplication.class, args);
 	}
 
